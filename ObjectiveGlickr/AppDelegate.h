d8e995dc20fed3663cdb5090c7877478bee83d3c
Ilter Cengiz <iltercengiz@yahoo.com>
2016-08-16 20:58:08 +0300

    Initial Commit

---
+++ ObjectiveGlickr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveGlickr
+//
+//  Created by Ilter Cengiz on 16/08/16.
+//  Copyright © 2016 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveGlickr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjectiveGlickr
+//
+//  Created by Ilter Cengiz on 16/08/16.
+//  Copyright © 2016 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
